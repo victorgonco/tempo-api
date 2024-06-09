cb17ef50c0ffb198301dd226031d68f89458bcec
goncoG1T <victorgoncarmo@gmail.com>
2024-06-09 12:57:39 -0300

    feat(Initial_commit): Application properties and spring boot starter.

---
+++ src/main/java/com/goncoG1T/tempoAPI/TempoApiApplication.java
@@ -0,0 +1,13 @@
+package com.goncoG1T.tempoAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goncoG1T/tempoAPI/TempoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goncoG1T.tempoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
