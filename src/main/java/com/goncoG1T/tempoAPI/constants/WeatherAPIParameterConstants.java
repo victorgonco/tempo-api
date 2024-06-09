c4142121f70c5e6d2fb252bbed188aded51d88cd
goncoG1T <victorgoncarmo@gmail.com>
2024-06-09 19:41:22 -0300

    feat(initial_structure): API constants and key.

---
+++ src/main/java/com/goncoG1T/tempoAPI/constants/WeatherAPIParameterConstants.java
@@ -0,0 +1,17 @@
+package com.goncoG1T.tempoAPI.constants;
+
+public final class WeatherAPIParameterConstants {
+
+    private WeatherAPIParameterConstants() {
+        //This class should have only one instance.
+    }
+
+    public static final String URI_REQUEST_PARAMS_KEY = "key=%s";
+    public static final String URI_REQUEST_PARAMS_ELEMENTS = "elements=datetime%2Caddress%2Ctempmax%2Ctempmin%2Ctemp%2Cfeelslike%2Chumidity%2Cprecipprob%2Cwindspeed%2Cdescription";
+    public static final String URI_REQUEST_PARAMS_CONTENT_TYPE = "contentType=json";
+    public static final String URI_REQUEST_PARAMS_TIME_FIELDS = "include=days%2Cobs%2Cremote%2Cfcst";
+    public static final String URI_REQUEST_PARAMS_UNIT = "unitGroup=metric";
+
+    public static final String URI_STRUCTURE_CONNECTOR = "&";
+
+}
