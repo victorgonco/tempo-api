0c60bc4168c2c23856c8ef560dcfff5fc9a16606
goncoG1T <victorgoncarmo@gmail.com>
2024-06-09 15:05:24 -0300

    feat(initial_structure): API Response DTOs.

--- src/main/java/com/goncoG1T/tempoAPI/dtos/responses/WeatherResponse.java
+++ src/main/java/com/goncoG1T/tempoAPI/dtos/responses/WeatherResponse.java
@@ -1,2 +1,13 @@
-package com.goncoG1T.tempoAPI.dtos.responses;public class WeatherResponse {
+package com.goncoG1T.tempoAPI.dtos.responses;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public record WeatherResponse(
+        @JsonProperty("resolvedAddress")
+        String address,
+        @JsonProperty("description")
+        String overallDesc,
+        List<WeatherResponseDay> days) {
 }
--- src/main/java/com/goncoG1T/tempoAPI/dtos/responses/WeatherResponseDay.java
+++ src/main/java/com/goncoG1T/tempoAPI/dtos/responses/WeatherResponseDay.java
@@ -1,2 +1,27 @@
-package com.goncoG1T.tempoAPI.dtos.responses;public class DaysWeatherResponse {
+package com.goncoG1T.tempoAPI.dtos.responses;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.LocalDate;
+
+
+public class WeatherResponseDay {
+
+    @JsonProperty("datetime")
+    private LocalDate date;
+    @JsonProperty("tempmax")
+    private String maxTemperature;
+    @JsonProperty("tempmin")
+    private String minTemperature;
+    @JsonProperty("temp")
+    private String actualTemperature;
+    @JsonProperty("feelslike")
+    private String actualTemperatureFeelsLike;
+    private String humidity;
+    @JsonProperty("precipprob")
+    private String precipitationProbability;
+    @JsonProperty("windspeed")
+    private String windSpeed;
+    private String description;
+
 }
